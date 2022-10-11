1fe8301c61b04d013217a5084085e0c408dc8f0d
Jihed Mastouri <jihedmastouri.inbox@gmail.com>
2022-10-11 08:56:40 +0100

    Docker-Compose DB working

--- ClientFolder/app.js
+++ ClientFolder/app.js
@@ -1,34 +1,29 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 8080;
-const mongoose = require('mongoose');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
 // const expressValidator = require('express-validator');
-const dotenv = require('dotenv');
-dotenv.config();
 
 //db connection
-mongoose.connect(
-    process.env.MONGO_URI,
-    { useNewUrlParser: true }
-)
-    .then(() => console.log('DB Connected'))
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .then(() => console.log("DB Connected"));
 
-mongoose.connection.on('error', err => {
-    console.log(`DB connection error: ${err.message}`)
+mongoose.connection.on("error", (err) => {
+  console.log(`DB connection error: ${err.message}`);
 });
 
-const foldersRoutes = require('./routes/folder');
-const clientsRoutes = require('./routes/client');
+const foldersRoutes = require("./routes/folder");
+const clientsRoutes = require("./routes/client");
 
 //middleware
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 // app.use(expressValidator());
-app.use('/folder', foldersRoutes);
-app.use('/client', clientsRoutes);
+app.use("/folder", foldersRoutes);
+app.use("/client", clientsRoutes);
 
-app.listen(port, () => {
-    console.log(`Server listening at port:${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening at port:${process.env.PORT}`);
 });
