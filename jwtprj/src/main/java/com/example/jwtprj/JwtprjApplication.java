8516adf1263c0631ed3bdf7dc1ec1484d8d56ee0
Jihed Mastouri <jihedmastouri.inbox@gmail.com>
2022-10-11 09:46:54 +0100

    Fix Eureka Clients

--- jwtprj/src/main/java/com/example/jwtprj/JwtprjApplication.java
+++ jwtprj/src/main/java/com/example/jwtprj/JwtprjApplication.java
@@ -6,6 +6,7 @@ import com.example.jwtprj.domain.User;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import java.util.ArrayList;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class JwtprjApplication {
 
     public static void main(String[] args) {
