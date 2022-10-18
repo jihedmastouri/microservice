818cffcb13dba287b2379500c725dafbbb3c5ae2
Jihed Mastouri <jihedmastouri.inbox@gmail.com>
2022-10-18 19:33:55 +0100

    spring works in docker

---
+++ mvn.sh
@@ -0,0 +1,13 @@
+cd jwtprj
+mvn clean install -DskipTests
+cd ..
+
+cd Constats
+mvn clean install -DskipTests
+cd ..
+
+cd EurekaServer
+mvn clean install -DskipTests
+cd ..
+
+echo "Build complete"
