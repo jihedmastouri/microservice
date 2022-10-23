d213ee65c424403cb25928a368f89b7ad95cd61d
Jihed Mastouri <jihedmastouri.inbox@gmail.com>
2022-10-24 00:08:18 +0100

    Zuul reg in Eureka

--- ApiGateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ ApiGateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -2,6 +2,8 @@ package com.example.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
