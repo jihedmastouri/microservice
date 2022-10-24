596cb4fe6798bc5cd60579dfb3036918b57ce275
Jihed Mastouri <jihedmastouri.inbox@gmail.com>
2022-10-24 13:21:26 +0100

    Add Filters classes to zuul

--- ApiGateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ ApiGateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -1,9 +1,14 @@
 package com.example.apigateway;
 
+import com.example.apigateway.filter.ErrorFilter;
+import com.example.apigateway.filter.PostFilter;
+import com.example.apigateway.filter.PreFilter;
+import com.example.apigateway.filter.RouteFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -14,4 +19,23 @@ public class ApiGatewayApplication {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
+	@Bean
+	public PreFilter preFilter() {
+		return new PreFilter();
+	}
+
+	@Bean
+	public PostFilter postFilter() {
+		return new PostFilter();
+	}
+
+	@Bean
+	public ErrorFilter errorFilter() {
+		return new ErrorFilter();
+	}
+
+	@Bean
+	public RouteFilter routeFilter() {
+		return new RouteFilter();
+	}
 }
