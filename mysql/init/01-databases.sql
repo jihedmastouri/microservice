21010758ccc4d99979be87155754ea1b9bcccdad
Jihed Mastouri <jihedmastouri.inbox@gmail.com>
2022-10-25 02:49:36 +0100

    database generation

--- mysql/init/01-databases.sql
+++ mysql/init/01-databases.sql
@@ -1,5 +1,10 @@
 # create databases
 CREATE DATABASE IF NOT EXISTS `constatapp`;
+CREATE DATABASE IF NOT EXISTS `collisionapp`;
+CREATE DATABASE IF NOT EXISTS `devisapp`;
+CREATE DATABASE IF NOT EXISTS `dossierapp`;
+CREATE DATABASE IF NOT EXISTS `rembourapp`;
+CREATE DATABASE IF NOT EXISTS `reparationapp`;
 
 # create root user and grant rights
 CREATE USER 'root'@'localhost' IDENTIFIED BY 'local';
